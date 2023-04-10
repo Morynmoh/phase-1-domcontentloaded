59155e10a6d7697d544f492b80dbc88346ba70bc
Maureen <morynmoh@gmail.com>
2023-04-10 15:33:24 +0300

    Completed Assignment - DOMContentLoaded

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+// document.addEventListener("DOMContentLoaded", function() {
+//   console.log("The DOM has loaded");
+// });
+//console.log("Maureen")
+//console.log("Maureen")
+document.addEventListener("DOMContentLoaded", function() {
+    console.log(document.getElementById("text").innerHTML =  "This is really cool!");
+})
+
